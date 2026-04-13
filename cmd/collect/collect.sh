ba258176c29e1bd32b312fcc727cc64acc35663f
Terrence Ryan <tpryan@google.com>
2026-04-13 11:16:31 -0700

    chore: Simplify collect script; ignore collect binary
    Reduce cmd/collect/collect.sh to a single invocation for user 'tpryan', removing other user calls and sleep delays. Add cmd/collect/collect to .gitignore to prevent committing the built/packed collect binary.

--- cmd/collect/collect.sh
+++ cmd/collect/collect.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-# ./collect -user tpryan
-# sleep 60
-./collect -user tklopfenstein
-sleep 60
-./collect -user ivanmkc
-sleep 60
-./collect -user manchan
-sleep 60
-./collect -user emilyball
-sleep 60
-./collect -user sfkrieger
-sleep 60
-./collect -user shahins
+./collect -user tpryan
